bbc431176ff1adc51d1b044b197189cb9a3cfdab
duc <adamvu2018@gmail.com>
2024-12-11 15:43:19 +0700

    update net utils

--- ad_net.py
+++ ad_net.py
@@ -30,7 +30,7 @@ def ad_download_file(url, file_to_write):
     urllib.request.urlretrieve(url, file_to_write)
 
 
-def ad_download_page(url, decode_utf8=True):
+def ad_download_page(url, decode_utf8=True, encoding='utf-8'):
     """
     Download a page
 
@@ -46,7 +46,7 @@ def ad_download_page(url, decode_utf8=True):
     """
     resp = urllib.request.urlopen(url)
     if decode_utf8:
-        return resp.read().decode('utf-8')
+        return resp.read().decode(encoding)
     else:
         return resp.read()
 
@@ -101,12 +101,36 @@ def ad_request_json(url):
         return None;
 
 
-def ad_post_request(logger, url, data):
-    headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
+def ad_post_request(logger, url, data, add_headers = {}):
+    headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/111.0'
+    }
+    headers.update(add_headers)
     response = rq.post(
         url,
         headers=headers,
         data=data)
+    if response.ok:
+        return response.json()
+    else:
+        try:
+            # Attempt to parse the error response as JSON
+            error_json = response.json()
+            logger.error("ad_post_request.Error JSON:")
+            logger.error(error_json)
+        except ValueError:
+            # If parsing as JSON fails, print the raw content
+            logger.error("ad_post_request.Error Content:")
+            logger.error(response.text)
+        return None
+
+
+def ad_get_request_json(logger, url, data, headers = {}):
+    response = rq.get(
+        url,
+        headers=headers)
     if response.ok:
         return response.json()
     else:
